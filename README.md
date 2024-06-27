511953ae8a8657b191e94b842caa69a847ce2e5d
Cory Latschkowski <koree@redhat.com>
2024-06-27 15:07:30 -0600

    update: configmap config (#6)
    * update: init
    * update: README.md
    
    ---------
    
    Co-authored-by: Carl Mes <carl.mes@gmail.com>

--- README.md
+++ README.md
@@ -2,7 +2,23 @@
 
 This repo helps to quickly configure Titon w/ vllm
 
-## Quick Start
+## Obtaining Models from Hugging Face
+
+Most of the models are available on model registry sites, the primary location for most of these is [Hugging Face](https://huggingface.co), which contains the models that are compatible with the inference server backend ([such as vLLM](https://docs.vllm.ai/en/stable/models/supported_models.html)).
+
+While most of these models are open, they are gated - meaning that a user must register for a Hugging Face account, and agree to each specific models terms and conditions before being able to download these.
+
+In order to automate the process when the inference serving container starts up, a [user access token](https://huggingface.co/docs/hub/security-tokens) must be generated and supplied as an environment variable.
+
+Steps to obtain the Hugging Face Hub Token:
+
+- Sign up for an account at [huggingface.co](https://huggingface.co), and login
+- Navigate to your user profile, settings, Access Tokens
+- Create a new access token, and copy the details to a safe location
+
+> Note: vLLM requires models in [Hugging Face Transformer Format](https://docs.vllm.ai/en/stable/models/supported_models.html#supported-models), this is why we pick Llama-2-7b-hf over Llama-2-7b, which doesn't have the model in a supported format for serving using vLLM. The Llama models can alternatively be downloaded directly from Meta by following these instructions: https://github.com/meta-llama/llama#download, however they will not be in the correctly supported format.
+
+## Quick Start Deployment
 
 ```sh
 # deployment with aws gp2/3
@@ -11,7 +27,17 @@ until oc apply -k bootstrap/overlays/default; do : ; done
 # deployment with odf storage
 until oc apply -k bootstrap/overlays/odf; do : ; done
 
+# download model
+# oc rsh deploy/triton-vllm-inference-server
+# . /scripts/init.sh; model_download
+```
+
+Add your Hugging Face Hub Token credentials to the secret:
+
+```sh
 # add hugging face secret to env
+oc -n llm-journey apply -f deployment/hf-secret.yaml
+oc set env -n llm-journey --from=secret/hugging-face-info deploy/triton-vllm-inference-server
 ```
 
 ## Uninstall
--- deployment/init.sh
+++ deployment/init.sh
@@ -15,7 +15,7 @@ create_trition_examples(){
   )
 }
 
-vllm_download_model_from_huggingface(){
+download_model_from_huggingface(){
   which huggingface-cli || return
   [ -n "${HUGGING_FACE_HUB_TOKEN}" ] || return
   [ -n "${HUGGING_FACE_MODEL}" ] || return
@@ -28,38 +28,70 @@ vllm_download_model_from_huggingface(){
     "${HUGGING_FACE_MODEL}"
 }
 
-vllm_generate_model_config(){
-  MODEL_PATH="${MODEL_REPOSITORY}/vllm_model"
-  [ -d "${MODEL_PATH}/1" ] || mkdir -p "${MODEL_PATH}/1"
-
-  print_config_pbtxt > "${MODEL_PATH}/config.pbtxt"
-  print_config_model > "${MODEL_PATH}/1/model.json"
+model_download(){
+  download_model_from_huggingface
 }
 
-vllm_curl(){
-  curl -X POST localhost:8000/v2/models/vllm_model/generate -d '{"text_input": "What is Triton Inference Server?", "parameters": {"stream": false, "temperature": 0}}'
+model_generate_config(){
+  MODEL_PATH="${MODEL_CACHE}/${HUGGING_FACE_MODEL}"
+  MODEL_CONFIG="${MODEL_REPOSITORY}/vllm_model"
+
+  [ -d "${MODEL_PATH}" ]        || return
+  [ -e /scripts/config.pbtxt ]  || return
+  [ -e /scripts/model.json ]    || return
+  [ -e "${MODEL_CONFIG}/1" ]    && return
+
+  [ -d "${MODEL_CONFIG}/1" ] || mkdir -p "${MODEL_CONFIG}/1"
+
+  cp /scripts/config.pbtxt  "${MODEL_CONFIG}/"
+  cp /scripts/model.json  "${MODEL_CONFIG}/1"
+  sed -i 's#../models/meta-llama/Llama-2-7b-hf#'"${MODEL_PATH}"'#g' "${MODEL_CONFIG}/1/model.json"
+
+  # print_config_pbtxt_via_curl > "${MODEL_PATH}/config.pbtxt"
+  # print_config_model_via_echo > "${MODEL_PATH}/1/model.json"
 }
 
-print_config_pbtxt(){
-  URL="https://raw.githubusercontent.com/redhat-na-ssa/demo-llm-journey/main/deployment/config.pbtxt"
-  echo "# see ${URL}"
-  curl -sL "${URL}"
+model_test(){
+  # see https://github.com/vllm-project/vllm/blob/736ed388492c5c10deb7522637a94c041f163f48/vllm/sampling_params.py#L38
+  URL=${URL:-localhost:8000/v2/models/vllm_model/generate}
+  
+  PARAMS='"max_tokens": "200", "temperature": 0.1, "stream": false'
+  INPUT=${1:-Who is Red Hat}
+
+  DATA='{"text_input": "'"${INPUT}"'", "parameters": {'"${PARAMS}"'}}'
+  curl -s -X POST "${URL}" -d "${DATA}"
 }
 
-print_config_model(){
-    MODEL_PATH="${MODEL_CACHE}/${HUGGING_FACE_MODEL}"
+# vllm_client(){
+#   URL=https://github.com/awslabs/data-on-eks/raw/main/gen-ai/inference/vllm-nvidia-triton-server-gpu/triton-client/prompts.txt
+#   curl -sLO "${URL}"
+#   URL=https://github.com/awslabs/data-on-eks/raw/main/gen-ai/inference/vllm-nvidia-triton-server-gpu/triton-client/triton-client.py
+#   curl -sLO "${URL}"
 
-cat << JSON
-{
-  "model": "${MODEL_PATH}",
-  "disable_log_requests": "true",
-  "gpu_memory_utilization": 0.8,
-  "enforce_eager": "true",
-  "max_model_len": 4096
-}
-JSON
-}
+#   python3 triton-client.py
+# }
 
-[ -d "${MODEL_CACHE}/${HUGGING_FACE_MODEL}" ] && vllm_generate_model_config
+# print_config_pbtxt_via_curl(){
+#   URL="https://raw.githubusercontent.com/redhat-na-ssa/demo-llm-journey/main/deployment/config.pbtxt"
+#   echo "# see ${URL}"
+#   curl -sL "${URL}"
+# }
+
+# print_config_model_via_echo(){
+#     MODEL_PATH="${MODEL_CACHE}/${HUGGING_FACE_MODEL}"
+
+# cat << JSON
+# {
+#   "model": "${MODEL_PATH}",
+#   "disable_log_requests": "true",
+#   "gpu_memory_utilization": 0.8,
+#   "enforce_eager": "true",
+#   "max_model_len": 4096
+# }
+# JSON
+# }
+
+model_download
+model_generate_config
 
 echo "Init Complete"
